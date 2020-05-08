dcad4b532224e29ed57ff032624eb79e938d3447
robertsahlin <robert.sahlin@gmail.com>
2020-05-08 17:31:42 +0200

    reactive mainverticle

--- converter/src/main/java/org/meshr/converter/MainVerticle.java
+++ converter/src/main/java/org/meshr/converter/MainVerticle.java
@@ -7,18 +7,27 @@ package org.meshr.converter;
  * in the LICENSE file.
  */
 
-import io.vertx.core.AbstractVerticle;
+//import io.vertx.core.AbstractVerticle;
+import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.Promise;
-import io.vertx.config.ConfigRetriever;
+import io.reactivex.Single;
+import io.reactivex.Observable;
+//import io.vertx.config.ConfigRetriever;
+import io.vertx.reactivex.config.ConfigRetriever;
+
 import org.meshr.converter.http.HttpServerVerticle;
 import org.meshr.converter.transform.TransformVerticle;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -30,9 +39,32 @@ public class MainVerticle extends AbstractVerticle {
     }
 
     @Override
-    public void start(Promise<Void> promise) throws Exception { 
+    //public void start(Promise<Void> promise) throws Exception { 
+        public void start(Future<Void> future) throws Exception { 
         LOG.info("MainVerticle deployed");
 
+        ConfigRetriever retriever = ConfigRetriever.create(vertx);
+        Observable<String> verticleNames = Observable.just(
+            HttpServerVerticle.class.getName(),
+            TransformVerticle.class.getName()
+        );
+        
+        retriever.rxGetConfig()
+            .flatMap(config -> verticleNames
+                    .flatMapSingle(verticleName -> vertx.rxDeployVerticle(verticleName, new DeploymentOptions().setConfig(config)))
+                    .toList()
+            )
+            .subscribe((rec, err) -> {
+                if (rec != null) {
+                  future.complete();
+                } else {
+                  future.fail(err);
+                }
+              });
+        
+
+
+        /*
         ConfigRetriever retriever = ConfigRetriever.create(vertx);
             retriever.getConfig(
                 config -> {
@@ -51,16 +83,15 @@ public class MainVerticle extends AbstractVerticle {
                             }
                         });
                     }
-            });
+            });*/
+
     }
 
-    private Future<Void> deployHelper(String name, JsonObject config){
-        final Future<Void> future = Future.future();
-        vertx.deployVerticle(
-            name,
-            new DeploymentOptions()
-            .setConfig(config),
-            res -> {
+    //private Future<Void> deployHelper(String name, JsonObject config){
+        private Single<String> deployHelper(String name, JsonObject config){
+        //final Future<Void> future = Future.future();
+        return vertx.rxDeployVerticle(name, new DeploymentOptions().setConfig(config));
+            /*res -> {
                 if(res.failed()){
                     LOG.error("Failed to deploy verticle " + name);
                     future.fail(res.cause());
@@ -70,6 +101,6 @@ public class MainVerticle extends AbstractVerticle {
                 }
             }
         );
-        return future;
+        return future;*/
     }
 }
