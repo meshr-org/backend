9de48dbf8f6c76d5805309df702784696ee2236c
robertsahlin <robert.sahlin@gmail.com>
2020-07-07 11:03:23 +0200

    fix transform

--- transform/com.google.analytics.v2.Event/index.js
+++ transform/com.google.analytics.v2.Event/index.js
@@ -30,35 +30,41 @@ const express = require('express');
 const {PubSub} = require('@google-cloud/pubsub');
 const pubsub = new PubSub();
 
+const uuidv4 = require('uuid/v4');
+
 const queryStringParser = require('query-string');
 
 // Create an Express object
 const app = express();
 app.set('trust proxy', true);
-app.post('/topic/:topic/', post);
+app.post('/topic/:topic/', apiPost);
 exports.com_google_analytics_v2_Event = app;
 
 function transform(request){
     var messages = [];
     var payloads = [];
-    console.log(Buffer.from(request.body.message.data, 'base64').toString().trim().replace(/\n/g, "\\n"));
+    //console.log(Buffer.from(request.body.message.data, 'base64').toString().trim().replace(/\n/g, "\\n"));
     const data = JSON.parse(Buffer.from(request.body.message.data, 'base64').toString().trim().replace(/\n/g, "\\n"));
     const attributes = request.body.message.attributes;
 
     if (data.payload) {
         data.payload.split("\n").forEach(function(e) {
-            //var tmp = Object.assign(queryStringParser(data.queryString), queryStringParser(e));
-            payloads.push(Object.assign(queryStringParser.parse(data.queryString), queryStringParser.parse(e)));
+            console.log(data.queryString);
+            payloads.push(Object.assign(queryStringParser.parse(e), data.queryString));
         });
     } else {
-        payloads.push(queryStringParser.parse(data.queryString));
+        payloads.push(data.queryString);
     }
 
     /*** START transformation ***/ 
-
     payloads.forEach(function(payload) {
         var message = {};
-        message.attributes = typeof attributes  !== 'undefined' ?  attributes  : {};
+        
+        message.attributes = typeof attributes  !== 'undefined' ?  Object.assign({}, attributes)  : {};
+        message.attributes.transformTimestamp = new Date().toISOString();
+        message.attributes.transformUuid = uuidv4();
+        message.attributes.transformTopic = request.params.topic;
+
         message.data = {
             version : payload.v,
             event_name : payload.en,
@@ -93,38 +99,6 @@ function transform(request){
         messages.push(message);
     });
     
-    /*
-    message.data = {
-        version : data.v,
-        items : Object.keys(data)
-            .filter( key => key.match(/^pr([0-9]{1,3})/) )
-            .reduce(function (r, a) {
-                r.push(data[a]);
-                return r;
-            },[])
-            .map(x => {
-                var y = x.split('~').reduce(function (r, a) {
-                    r[a.slice(0,2)] = a.slice(2);
-                    return r;
-                },{});
-                return {
-                    id : y.id,
-                    brand : y.br,
-                    name : decodeURIComponent(y.nm),
-                    variant : y.va,
-                    category : y.ca,
-                    category2 : y.v0,
-                    category3 : y.v1,
-                    category4 : y.v2,
-                    category5 : y.v3,
-                    price : y.pr,
-                    quantity : y.qt,
-                    list_id : y.li,
-                    list_name : decodeURIComponent(y.ln)
-                };
-            })
-    };
-    */
     /*** STOP transformation ***/
     
     return messages;
@@ -133,34 +107,27 @@ function transform(request){
 async function publish(request, response){
     // Pubsub topics to publish message on
     var topic = request.params.topic;
-    var topics =[topic];
+    //var topics =[topic];
 
     //transform request payload
     var messages = transform(request); 
     
     // Publish to topic
-    let messageIds = await Promise
-        .all(
-            topics.map(currentTopic => { 
-                console.log(currentTopic);
-                messages.forEach(function(message) {
-                    console.log(message);
-                    pubsub
-                        .topic(currentTopic)
-                        .publish(Buffer.from(JSON.stringify(message.data)), message.attributes)
-                
-                })
-            }))
-            //}).flat())
+    let messageIds = await Promise.all(
+        messages.map(async message => { 
+            return pubsub
+                .topic(topic)
+                .publish(Buffer.from(JSON.stringify(message.data)), message.attributes)
+        }))
         .catch(function(err) {
             console.error(err.message);
             response.status(400).end(`error when publishing data object to pubsub`);
         });
-    //console.log(messageIds);
+    console.log(messageIds);
 }
 
 // Collect pubsub push request (POST), transform it and publish data on pubsub
-async function post(request, response) {
+async function apiPost(request, response) {
     if (!request.body) {
         const errorMsg = 'no Pub/Sub message received';
         console.error(`error: ${errorMsg}`);
